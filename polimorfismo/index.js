7a42cb0e4e81553acc2fde3f0df938dcab2f4d7c
Roberto Fortes <robertbernd23@gmail.com>
2021-07-12 08:18:21 -0300

    Removendo bug, criando logica metodo verSaldo

--- polimorfismo/index.js
+++ polimorfismo/index.js
@@ -5,10 +5,14 @@ function Conta(agencia, conta, saldo) {
 }
 
 Conta.prototype.sacar = function(valor) {
-    if(valor > (this.saldo + this.limite)) {
-    console.log(`Saldo insuficiente: ${this.saldo}`);
-    return;
-}
+    if(valor > this.saldo) {
+      console.log(`Saldo insuficiente: ${this.saldo}`);
+      return;
+    }
+    
+    this.saldo -= valor;
+    this.verSaldo();
+  };
 
 Conta.prototype.depositar = function(valor) {
   this.saldo += valor;
@@ -16,5 +20,8 @@ Conta.prototype.depositar = function(valor) {
 };
 
 Conta.prototype.verSaldo = function() {
-
-};
+    console.log(
+      `Ag/c: ${this.agencia}/${this.conta} | ` +
+      `Saldo: R$${this.saldo.toFixed(2)}`
+    );
+  };
