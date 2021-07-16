84bb57e38535b1617a5af17ec800df5e7538d679
Roberto Fortes <robertbernd23@gmail.com>
2021-07-16 05:27:17 -0300

    Criando metodos e definindo prototypes

--- factory_prototypes/index.js
+++ factory_prototypes/index.js
@@ -1,5 +1,28 @@
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  },
+};
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo.`);
+  },
+};
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo.`);
+  },
+};
+
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
 function criaPessoa(nome, sobrenome) {
-  
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome }
+  });
 }
 
 const p1 = criaPessoa('Roberto', 'Fortes');
